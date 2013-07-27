0db474680f8a0fadf2b79eae14a681f739e6214d
Per Lindstrand <fryingpanjoe@gmail.com>
2013-07-27 13:31:36 +0200

    FizzBuzz in D
    Added a FizzBuzz implementation for the D programming language

---
+++ fizzbuzz/d/fizzbuzz.d
@@ -0,0 +1,16 @@
+import std.stdio;
+
+void main()
+{
+  for (int i = 1; i < 101; ++i) {
+    if (i % 15 == 0) {
+      writeln("FizzBuzz");
+    } else if (i % 3 == 0) {
+      writeln("Fizz");
+    } else if (i % 5 == 0) {
+      writeln("Buzz");
+    } else {
+      writeln(i);
+    }
+  }
+}
